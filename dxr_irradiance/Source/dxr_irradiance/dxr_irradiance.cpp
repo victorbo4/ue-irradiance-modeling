71a37d51f4502b10a41e6b443f70fc54d48b934f
Víctor Blázquez <vicalo8673@gmail.com>
2025-08-12 20:48:33 +0200

    Delete dxr_irradiance directory

--- dxr_irradiance/Source/dxr_irradiance/dxr_irradiance.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "dxr_irradiance.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, dxr_irradiance, "dxr_irradiance" );
